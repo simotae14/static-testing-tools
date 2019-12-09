63c70c0428c8b2c5a3b8ccc82e6a6e3126becbf6
Simone Taeggi <startae14@gmail.com>
2019-12-09 23:38:04 +0100

    use pre-built eslint configuration

--- src/example.js
+++ src/example.js
@@ -1,14 +1,11 @@
-// eslint-disable-next-line strict
-'use strict';
-
 const username = 'freddy'
-typeof name === 'strng'
+typeof username === 'string'
 
-if (!'serviceWorker' in navigator) {
+if (!('serviceWorker' in navigator)) {
   // you have an old browser :-(
 }
 
 const greeting = 'hello'
-console.log('${greting} world!')
+console.log(`${greeting} world!`)
 
 [(1, 2, 3)].forEach(x => console.log(x))
